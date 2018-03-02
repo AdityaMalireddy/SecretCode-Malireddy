fc06ca183aaeb2ec82fd8adeef67e5d7e33f9dae
Malireddy,Aditya Jytohiswaroop <s530860@ol10018m.local>
2018-03-01 18:26:12 -0600

    Initial Commit

---
+++ SecretCode-Malireddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecretCode-Malireddy
+//
+//  Created by Malireddy,Aditya Jytohiswaroop on 3/1/18.
+//  Copyright © 2018 Malireddy,Aditya Jytohiswaroop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
